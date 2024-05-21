9bff2933e46d7b89d69666d964224968c04d4117
Frablock <53878484+Frablock@users.noreply.github.com>
2024-05-21 20:08:47 +0200

    Update Scenario.java

--- src/Scenario.java
+++ src/Scenario.java
@@ -3,6 +3,8 @@ import localization.*;// Utilitaire de traduction
 import modele.*;// Modèle de Base pour MVC
 import tri.*;//Algorithme de tri utiles
 
+import java.util.ArrayList;
+
 public class Scenario {
     public static void main(String[] args) {
         testLocalization();
@@ -12,13 +14,13 @@ public class Scenario {
 
     private static void testLocalization() {
         Localization.loadLocales("fr");
-        System.out.println("Français : "+Localization.getL("header.welcome")+" Jean Dupont\n"+Localization.getL("header.whatsee"));
+        System.out.println("Français : "+Localization.getL("header.welcome")+" Jean Dupont\n"+Localization.getL("header.whatsee")+"\n");
         Localization.loadLocales("en");
-        System.out.println("English : "+Localization.getL("header.welcome")+" Jean Dupont\n"+Localization.getL("header.whatsee"));
+        System.out.println("English : "+Localization.getL("header.welcome")+" Jean Dupont\n"+Localization.getL("header.whatsee")+"\n");
     }
 
     private static void testModele() {
-        System.out.println("à faire");
+        Commune c = new Commune(545454, "Commune de test 1", new ArrayList<Commune>());
     }
     private static void testTri() {
         System.out.println("à faire");
