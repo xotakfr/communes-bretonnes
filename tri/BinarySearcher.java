6ca4765878bd2f0d7e8f6d4719d0f43dc57d6c34
Frablock <franon1.frablock29@gmail.com>
2024-05-21 15:19:33 +0200

    Recherche renvoie un ID, existsi un boolean si l'objet existe ou pas

--- tri/BinarySearcher.java
+++ tri/BinarySearcher.java
@@ -13,7 +13,7 @@ public class BinarySearcher<T> {
      * @param arr ArrayListe of Type
      * @param obj object (Type) to search
      */
-    public static boolean search(ArrayList<T> arr, T obj) {
+    public static boolean exists(ArrayList<T> arr, T obj) {
         int start = 0;
         int end = arr.length-1;
         int mil;
@@ -33,5 +33,32 @@ public class BinarySearcher<T> {
 
         }
         return found;
+    }
+
+        /**
+     * Return the ID of the given parameter
+     * @param arr ArrayListe of Type
+     * @param obj object (Type) to search
+     */
+    public static int search(ArrayList<T> arr, T obj) {
+        int start = 0;
+        int end = arr.length-1;
+        int mil;
+
+        int ret = -1;
+
+        while (!found && start <= end) {
+            mil = (start+end)/2;
+
+            if (arr.get(mil).compareTo(obj)==0) {
+                ret = mil;
+            } else if (obj.compareTo(arr.get(mil))<0) {
+                end = mil -1;
+            } else if (obj.compareTo(arr.get(mil))>0) {
+                start = mil + 1;
+            }
+
+        }
+        return mil;
     }
 }
