c484e01f00c94f91fa0b99456f9339df0ba43384
Frablock <53878484+Frablock@users.noreply.github.com>
2024-05-21 20:01:21 +0200

    Update Scenario.java

--- src/Scenario.java
+++ src/Scenario.java
@@ -3,8 +3,6 @@ import localization.*;// Utilitaire de traduction
 import modele.*;// Modèle de Base pour MVC
 import tri.*;//Algorithme de tri utiles
 
-import java.util.stream;
-
 public class Scenario {
     public static void main(String[] args) {
         testLocalization();
@@ -14,9 +12,9 @@ public class Scenario {
 
     private static void testLocalization() {
         Localization.loadLocales("fr");
-        System.out.println("Français : "+Localization.getL("header.welcome")+" Jean Dupont");
+        System.out.println("Français : "+Localization.getL("header.welcome")+" Jean Dupont\n"+Localization.getL("header.whatsee"));
         Localization.loadLocales("en");
-        System.out.println("English : "+Localization.getL("header.welcome")+" Jean Dupont");
+        System.out.println("English : "+Localization.getL("header.welcome")+" Jean Dupont\n"+Localization.getL("header.whatsee"));
     }
 
     private static void testModele() {
