5f26a0780e9c159e275542013ded9e7f352f21d9
Frablock <franon1.frablock29@gmail.com>
2024-05-27 17:22:29 +0200

    Update TestDriver.java

--- src/TestDriver.java
+++ src/TestDriver.java
@@ -1,4 +1,18 @@
-public class TestDriver extends modele.DAO<Integer> {
+public class TestDriver {
+    private String url = "jdbc:mysql://127.0.0.1:3306/BDSAE";
+    private String user = "user";
+    private String password = "password";
+
+    protected Connection getConnection() {
+        try {
+            class.forname("com.mysql.cj.jdbc.Driver");
+
+            return DriverManager.getConnection("url", user, password);
+        } catch (ClassNotFoundException e) {
+            System.err.println(e.stackTrace());
+        }
+    }
+
     public static void main (String[] args) {
         getConnection();
     }
