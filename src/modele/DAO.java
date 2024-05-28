ddbca76ce3ef8716775c53a93ea04b8b26d13731
Frablock <53878484+Frablock@users.noreply.github.com>
2024-05-28 09:30:14 +0200

    privte to protected

--- src/modele/DAO.java
+++ src/modele/DAO.java
@@ -25,7 +25,7 @@ public abstract class DAO<T> {
      * Utilitaire de lancement d'une requête SQL
      * Voir exemple implémentation dans CommuneDAO
      */
-    private abstract List<T> runSQLQuery(Connection connection, String sql) throws Exception;
+    protected abstract List<T> runSQLQuery(Connection connection, String sql) throws Exception;
 
     public abstract List<T> findAll();
     public abstract T findById(long id);
