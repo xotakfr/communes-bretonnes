478dd001a90f4d3b60eee23919715fa4300ae610
Frablock <53878484+Frablock@users.noreply.github.com>
2024-05-28 09:20:53 +0200

    Ajout d'utilitaires de compilation

---
+++ ws/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CLASSSPATH="../class:../lib/*.jar"
+
+if [[ -n "$PATH_TO_FX" ]]; then
+    file=$(echo zenity --entry --title="Entrer le nom de votre fichier à lancer")
+    java --module-path $PATH_TO_FX --add-modules javafx.controls,javafx.base,favafx.fxml "$file"
+else
+  echo "\$PATH_TO_FX n'est pas défini, veuillez le définir dans votre .bashrc"
+  echo "Ajoutez cette ligne dans votre .bashrc"
+  echo "export PATH_TO_FX=\"Répértoires vers /javafx-sdk-22.0.1/lib\""
+fi
+
+
