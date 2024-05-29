68472a5a4b88f366e3a676981f11bb54d2bb90f8
XotakFR <j.l.emeraud@gmail.com>
2024-05-29 11:33:11 +0200

    remove build scripts

--- ws/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-javac --module-path ../lib --add-modules javafx.controls,javafx.base,javafx.fxml ../src/*.java ../src/*/*.java -d ../class
--- ws/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-CLASSSPATH="../class:../lib/*.jar"
-
-java --module-path ../lib --add-modules javafx.controls,javafx.base,javafx.fxml "$1"
