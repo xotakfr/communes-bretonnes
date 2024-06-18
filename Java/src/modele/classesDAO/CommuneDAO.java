53d15f82156815fe936d0f7d930d7826f703d1d9
Frablock <53878484+Frablock@users.noreply.github.com>
2024-06-18 19:22:54 +0200

    Update CommuneDAO.java

--- Java/src/modele/classesDAO/CommuneDAO.java
+++ Java/src/modele/classesDAO/CommuneDAO.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.sql.PreparedStatement;
 
 import modele.classesModele.Commune;
 
