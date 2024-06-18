145167280ee4177a2ea3093b8766c33780c87e10
unknown <nathan.guheneuf@gmail.com>
2024-06-18 17:32:11 +0200

    documentated

--- Java/src/view/scenes/LoginScene.java
+++ Java/src/view/scenes/LoginScene.java
@@ -11,18 +11,27 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.sql.Connection;
 
+/**
+ * Implémente UIScenes et charge la scène correspondant au menu de connexion.
+ * @author Nathan Guheneuf-Le Brec, Inaki Gomez--Jego, Jean-Louis Emeraud, François Patinec-Haxel
+ * @see UIScenes 
+ */
 public class LoginScene implements UIScenes {
 
+    /**
+     * Récupère la scène associée au menu de connexion de l'application et
+     * l'applique au stage de l'application
+     * @param stage Le stage de l'application
+     */
     public static void loadScene(Stage stage) {
         try {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(LoginScene.class.getResource("../../login_screen.fxml"));
             GridPane pane = loader.load();
-
             Scene scene = new Scene(pane);
             stage.setScene(scene);
-
-        } catch (IOException e) {
+        } 
+        catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
--- Java/src/view/scenes/UIScenes.java
+++ Java/src/view/scenes/UIScenes.java
@@ -2,6 +2,16 @@ package view.scenes;
 
 import javafx.stage.Stage;
 
+/**
+ * Interface pour les classes de chargement de scènes.
+ * Elle définit la signature d'une méthode permettant de modifier la scène
+ * du stage de l'application en récupérant un fichier .fxml
+ * @author Nathan Guheneuf-Le Brec, Inaki Gomez--Jego, Jean-Louis Emeraud, François Patinec-Haxel
+ */
 public interface UIScenes {
+    /**
+     * Signature de la méthode utilisée par récupérer une scène et l'appliquée au stage
+     * @param stage Le stage de l'application
+     */
     static void loadScene(Stage stage) {}
 }
