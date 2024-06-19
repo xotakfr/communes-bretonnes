99a2a939433980f3c8c52ed3b59332f390215b83
unknown <nathan.guheneuf@gmail.com>
2024-06-19 19:58:50 +0200

    reverted, i frogot smt

--- Java/src/modele/dao/AnneeDAO.java
+++ Java/src/modele/dao/AnneeDAO.java
@@ -2,56 +2,39 @@ package modele.dao;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.sql.PreparedStatement;
 
 
 import modele.data.Annee;
-import modele.data.Commune;
 
 /**
  * Data Access for Annee
  * Use all fonction in DAO for the Annee objects
  */
-public class AnneeDAO extends DAO<Annee> {
+public class AnneeDAO {
     /** Filtre actuel - Voir comparableTo et SwitecherFilter */
     private static String currentFilter = "annee";
     /** Liste des filtres possibles */
     private static String[] filtersList = new String[]{"annee", "tauxInflation"};
 
 
-    protected static ArrayList<Annee> runSQLQuery(Connection connection, String sql) throws Exception {
-        ArrayList<Annee> results = new ArrayList<Annee>();
+    protected static Annee runSQLQuery(Connection connection, String sql) throws Exception {
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery(sql);
-        Annee com;
+        Annee com = new Annee(0,0f);
 
+        
         while (resultSet.next()) {
-            com = new Annee(
-                    resultSet.getInt(0), 
-                    resultSet.getFloat(1)
-                );
-            results.add(com);
+            com = new Annee(resultSet.getInt(0), resultSet.getFloat(1));
         }
         
         resultSet.close();
         statement.close();
-        return results;
+        return com;
     }
 
-    public  ArrayList<Annee> findAll(Connection c) {
-        ArrayList<Annee> arr = new ArrayList<Annee>();
-        try {
-            arr = runSQLQuery(c, "SELECT * FROM Annee;");
-        } 
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        return arr;
-    }
-
-    public static ArrayList<Annee> getFromId(Connection co, int id) {
-        ArrayList<Annee> str = null;
+    public static Annee getFromId(Connection co, int id) {
+        Annee str = new Annee(0,0f);
 
         try {
             str = runSQLQuery(co, "SELECT * FROM Annee WHERE \"Annee.annee\" LIKE \""+id+"\";");
